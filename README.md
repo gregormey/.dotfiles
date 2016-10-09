7e7edb9afd72c1a72d4dc628dfab03f94c3b7462
Gregor Meyenberg <gregor@meyenberg.de>
2016-10-09 20:39:34 +0200

    Install command in ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # My .dotfiles
+
+## Install
+```bash
+bash <(curl https://raw.githubusercontent.com/gregormey/.dotfiles/master/install.sh)
+```
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 echo "Install pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
