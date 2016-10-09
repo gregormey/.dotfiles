f05a1a02d831ebabe19d005301eb24ec04250cd3
Gregor Meyenberg <gregor@meyenberg.de>
2016-10-09 20:35:51 +0200

    Install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+echo "Install pathogen"
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "Install Plugins"
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git 
+git clone https://github.com/szw/vim-maximizer.git 
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/jimenezrick/vimerl.git
+git clone https://github.com/vim-erlang/vim-erlang-omnicomplete.git
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
+
+echo "Create .vimrc"
+cd ~
+git clone https://github.com/gregormey/.dotfiles.git
+ln -s ~/.vimrc ~/.dotfiles/vim/.vimrc
+
+echo "Create tmux.conf"
+ln -s ~/.tmux.conf ~/.dotfiles/tmux/.tmux.conf
+
+echo "Set color theme"
+cd ~
+git clone https://github.com/hukl/Smyck-Color-Scheme.git
+mkdir -p ~/.vim/colors && \
+ln -s ~/.vim/colors/smyck.vim ~/Smyck-Color-Scheme/smyck.vim
+
+
